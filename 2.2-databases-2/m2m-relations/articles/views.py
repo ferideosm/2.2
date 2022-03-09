625816015d7dd962cf9c7f371d47e9906fa7aad5
feride <ferideosm@gmail.com>
2022-03-09 23:32:36 +0300

    last

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -5,20 +5,6 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
     articles = Article.objects.all()
-    print('articles ==', articles)
-    for a in articles:
-        print('a ==', a)
-        for scope in a.scopes.all():
-            print('scope ==', scope)
-            print('scope.id ==', scope.id)
-            print('scope.is_main ==', scope.is_main)
-            print('scope .tags==', scope.tag)
-
     context = {'object_list': articles}
     return render(request, template, context)
