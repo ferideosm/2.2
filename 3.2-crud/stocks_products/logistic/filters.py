00cbe444bfd7324f3a59c90e994589e2bf38ca55
feride <ferideosm@gmail.com>
2022-03-15 10:13:14 +0300

    3.2 Crud LAST

--- 3.2-crud/stocks_products/logistic/filters.py
+++ 3.2-crud/stocks_products/logistic/filters.py
@@ -3,10 +3,14 @@ from .models import Stock
 
 class ProductsFilter(django_filters.FilterSet):
     products = django_filters.NumberFilter(
-        field_name='products__id',
+        field_name='products__id',        
         lookup_expr='exact',
     )
-
+    product_name = django_filters.CharFilter(
+        field_name ='products__title',        
+        lookup_expr= 'contains',
+    )
+    
     class Meta:
         model = Stock
-        fields = ['address', 'products']
+        fields = ['address', 'products', 'product_name']
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -2,6 +2,7 @@ from re import search
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
 from logistic.models import Product, Stock, StockProduct
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -12,13 +13,17 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [SearchFilter]
-    search_fields = ['title', ]
+    search_fields = ['title', 'description']
+    pagination_class = LimitOffsetPagination
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     filter_backends = (DjangoFilterBackend,)
+    # filter_backends = [SearchFilter]
+    # search_fields = ['description', ]
     
-    filterset_fields = ['products', ]
+    filterset_fields = ['products', 'address', 'product_name' ]
     filterset_class= ProductsFilter
+    pagination_class = LimitOffsetPagination
